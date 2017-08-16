59f79b67dee20c32dcc088e5e42b2ed46057769b
siminino <victor.areas@corp.globo.com>
2017-08-16 11:25:28 -0300

    adding enzyme and youtube component tests

--- tests/YouTube_test.js
+++ tests/YouTube_test.js
@@ -5,26 +5,41 @@
  */
 
 import React from "react";
-import TestUtils from "react-addons-test-utils";
+
+import { shallow } from "enzyme";
 import chai from "chai";
+import chaiEnzyme from "chai-enzyme";
 
 import YouTube from "../src/YouTube";
 
+chai.use(chaiEnzyme());
 let expect = chai.expect;
 
 describe("YouTube", function () {
-  beforeEach(function () {
+  before(function () {
     this.videoID = "m_3CM04TM2g";
-    this.component = TestUtils.renderIntoDocument(
-      <YouTube videoID={this.videoID} />
-    );
   });
 
   it("should render nothing when no VideoID", function () {
-    const component = TestUtils.renderIntoDocument(
+    const wrapper = shallow(
       <YouTube />
     );
-    expect(component.textContent).to.be.empty;
-    expect(component.children).to.be.empty;
+    expect(wrapper).to.exist;
+    expect(wrapper.find("iframe")).to.not.exist;
+  });
+
+  it("should render iframe when VideoID was given", function () {
+    const wrapper = shallow(
+      <YouTube videoID={ this.videoID }/>
+    );
+    expect(wrapper.find("iframe")).to.exist;
+  });
+
+  it("should render iframe with expected url as source", function () {
+    const expectedURL = `https://www.youtube.com/embed/${this.videoID}`,
+      wrapper = shallow(
+        <YouTube videoID={ this.videoID }/>
+      );
+    expect(wrapper.find("iframe")).to.have.prop("src", expectedURL);
   });
 });
